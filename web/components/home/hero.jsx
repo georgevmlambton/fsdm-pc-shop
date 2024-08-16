d45b1f743b6b6b11fb137d902f62d31aec34dd8e
George Varghese <georgevmlambton@proton.me>
2024-08-16 10:10:08 -0400

    Test

--- web/components/home/hero.jsx
+++ web/components/home/hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <span className="fs-2">Ready to level up your device?</span>
             <br />
             <span className="fs-2 fw-bold">
-              LETS BUILD YOUR DREAM PCCC <br />
+              LETS BUILD YOUR DREAM PC <br />
               TOGETHER!
             </span>
           </p>
